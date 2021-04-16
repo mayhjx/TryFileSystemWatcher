a27fec6003233a52f2ac9807d366700e3bb946bc
马扬杰 <ma-yangjie@outlook.com>
2021-04-16 08:38:57 +0800

    Add project files.

---
+++ TryFileSystemWatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryFileSystemWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
