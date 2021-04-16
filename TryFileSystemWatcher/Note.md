d61c1325ea5d7c2e4ee1fab764c577c5b30b804d
马扬杰 <ma-yangjie@outlook.com>
2021-04-16 08:53:04 +0800

    文件或目录监视方法

---
+++ TryFileSystemWatcher/Note.md
@@ -0,0 +1,20 @@
+﻿## FileSystemWatcher
+
+可以使用FileSystemWatcher组件监视文件系统，并对文件系统的改变作出反应。
+通过使用FileSystemWatcher组件，在特定的文件或目录被创建、修改或删除时，可以快速和便捷地启动业务流程。
+例如，如果一组用户在合作处理一个存储在服务器共享目录下的文档时，可以使用FileSystemWatcher组件编写应用程序来监视对共享目录的更改情况。
+当检测到更改时，该组件可以运行处理过程，通过电子邮件通知每个用户。
+
+可以配置组件来监视整个目录及其内容，或特定目录下一个特定的文件或一组文件。
+若要监视所有文件中的更改，应将Filter属性设置为空字符串（""）；
+若要监视特定的文件，应将Filter属性设置为该文件的文件名（例如，若要监视文件MyDoc.txt中的更改，将Filter属性设置为"MyDoc.txt"）；
+也可以监视特定文件类型中的更改，例如若要监视文本文件中的更改，将Fillter属性设置为"*.txt"。
+
+【提示】不要忽略隐藏文件。
+
+可监视目录或文件中的若干种更改。
+例如，可监视文件或目录的Attributes、LastWrite的日期和时间或Size的更改。
+通过将FileSystemWatcher.NotifyFilter属性设置为NotifyFilters中的某个值，就可以实现这个目标。
+
+还可以监视文件或目录的重命名、删除或创建。
+例如，若要监视文本文件的重命名，可将Filter属性设置为"*.txt"，并调用一个WaitForChanged方法，并设置该方法中WatcherChangeTypes的值为Renamed。
--- TryFileSystemWatcher/Program.cs
+++ TryFileSystemWatcher/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.IO;
 
 namespace TryFileSystemWatcher
 {
-    class Program
+    class Watcher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 如果没有指定目录，则退出程序
+            if (args.Length != 1)
+            {
+                // 显示调用程序的正确方法
+                Console.WriteLine("usage: Watcher.ext(directory)");
+                return;
+            }
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = args[0];
+
+            // 监视LastAccess和LastWrite时间的更改以及文件或目录的重命名
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite |
+                NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+            // 只监视文本文件
+            watcher.Filter = "*.txt";
+
+            // 添加事件句柄
+            // 当由FileSystemWatcher所指定的路径中的文件或目录的
+            // 大小、系统属性、最后写时间、最后访问时间或安全权限
+            // 发生更改时，更改事件就会发生。
+            watcher.Changed += new FileSystemEventHandler(OnChange);
+            //文件或目录被创建时触发
+            watcher.Created += new FileSystemEventHandler(OnChange);
+            //文件或目录被删除时触发
+            watcher.Deleted += new FileSystemEventHandler(OnChange);
+            //文件或目录被重命名时触发
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // 开始监视
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press\'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
+        }
+
+        public static void OnChange(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"File: {e.FullPath} {e.ChangeType}");
+        }
+        public static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"File:{e.OldFullPath} renamed to {e.FullPath}");
         }
     }
 }
